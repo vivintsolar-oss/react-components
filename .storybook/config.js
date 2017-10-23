ca994d398bd9164d84dd4179a5c7abb1c9d9e30c
tcasey <hello@tcasey.me>
2017-10-23 12:41:29 -0600

    Publish
     - @vivintsolar-oss/web-vs-button@1.0.9
     - @vivintsolar-oss/web-vs-input@1.0.1

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,8 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   // require('./stories/index.js');
-  require('../packages/vs-button/story.js')
+  require('../packages/vs-button/story.js');
+  require('../packages/vs-input/story.js');
   // You can require as many stories as you need.
 }
 
--- packages/vs-input/story.js
+++ packages/vs-input/story.js
@@ -3,18 +3,31 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import VSInput from './src/index';
 
-const stories = storiesOf('Button', module);
+const stories = storiesOf('Input', module);
 
 stories.addDecorator(withKnobs);
 
 stories.add('Primary', () => {
-  return <VSInput disable={ boolean('Disabled', false) } />;
+  return (
+    <VSInput
+      disable={ boolean('Disabled', false) }
+      active={ boolean('Active', false) }
+      error={ text('Error', 'Uh Oh Spaghetti O\'s') }
+      label={ text('Label', 'Text Input') }
+    />
+  );
 });
 
 stories.add('Primary-Dark', () => {
   return (
     <div style={ { padding: '20px', backgroundColor: '#323A40' } }>
-      <VSInput disable={ boolean('Disabled', false) } />
+      <VSInput
+        dark={ boolean('Dark', true) }
+        disable={ boolean('Disabled', false) }
+        active={ boolean('Active', false) }
+        error={ text('Error', 'Uh Oh Spaghetti O\'s') }
+        label={ text('Label', 'Text Input') }
+      />
     </div>
   );
 });
